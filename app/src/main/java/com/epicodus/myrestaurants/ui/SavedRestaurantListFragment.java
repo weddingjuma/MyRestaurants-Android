6b7cef3e14e553d31aafb38af7f2f03c18478b6f
Evan Bishop <evan.bishop888@gmail.com>
2017-06-12 15:09:40 -0700

    successfully include mondays homework

---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -0,0 +1,92 @@
+package com.epicodus.myrestaurants.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.myrestaurants.Constants;
+import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.OnStartDragListener;
+import com.epicodus.myrestaurants.util.SimpleItemTouchHelperCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SavedRestaurantListFragment extends Fragment implements OnStartDragListener {
+    private FirebaseRestaurantListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    public SavedRestaurantListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_saved_restaurant_list, container, false);
+        ButterKnife.bind(this, view);
+        setUpFirebaseAdapter();
+        return view;
+    }
+
+    private void setUpFirebaseAdapter() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        Query query = FirebaseDatabase.getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
+                .child(uid)
+                .orderByChild(Constants.FIREBASE_QUERY_INDEX);
+
+        //  In line below, we change 6th parameter 'this' to 'getActivity()'
+        //  because fragments do not have own context:
+        mFirebaseAdapter = new FirebaseRestaurantListAdapter(Restaurant.class,
+                R.layout.restaurant_list_item_drag, FirebaseRestaurantViewHolder.class,
+                query, this, getActivity());
+
+        mRecyclerView.setHasFixedSize(true);
+
+        //In line below, we change 'this' to 'getActivity()' because fragments do not have own context:
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                mFirebaseAdapter.notifyDataSetChanged();
+            }
+        });
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+
+    @Override
+    //method is now public
+    public void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+}
