8db9e4a74f441992eda8cbf7a45a9cdd2d307594
Evan Bishop <evan.bishop888@gmail.com>
2017-06-12 08:59:14 -0700

    ...

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -22,13 +22,11 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
     private DatabaseReference mRestaurantReference;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
 
-    @Bind(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
